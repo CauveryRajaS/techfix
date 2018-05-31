2c00be899721518d01355bbe9fbd9dcc2a7d727a
Cauvery Raja S <cauverysiva@gmail.com>
2018-05-31 09:53:09 +0530

    second commit

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-    database: 'mongodb://cauverysiva:raja2533@ds159926.mlab.com:59926/cseapp',
+    database: 'mongodb://cauverysiva:raja2533@ds241530.mlab.com:41530/techfix',
     secret:'mysecret'
 }
 
@@ -9,4 +9,6 @@ module.exports = {
     database: 'mongodb://localhost:27017/techapp',
     secret:'mysecret'
 }*/
-  
+
+//techfix
+//<dbuser>:<dbpassword>@ds241530.mlab.com:41530/techfix
