68ddcdf5c1068965a4083efa95d6617140fce4f5
Cauvery Raja S <cauverysiva@gmail.com>
2018-07-15 11:40:32 +0530

    commiting

--- routes/users.js
+++ routes/users.js
@@ -61,11 +61,15 @@ router.post('/register', ( req, res, next) => {/*
         let newUser = new User({
             name:req.body.name,
             email:req.body.email,
-            contact:req.body.contact/*,
+            contact:req.body.contact,/*
             password:req.body.password,
             copy:req.body.copy,
             question:req.body.question,
             answer:req.body.answer*/
+            password:'',
+            copy:'',
+            question:'',
+            answer:''
         });
         User.addUser(newUser,(err,user) => {
             if(err) {
